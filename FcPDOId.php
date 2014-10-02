f71d5e77f3a63a2a51311bf89d50afc8aec802f7
Flavien Collomb <social@flaviencollomb.fr>
2014-10-02 13:31:01 +0200

    Create FcPDOId.php

---
+++ FcPDOId.php
@@ -0,0 +1,24 @@
+Class FcPDOId{
+    /**
+     * @param $database
+     * @return mixed
+     * @throws BadMethodCallException
+     */
+    public static function get($database){
+        if(method_exists(get_class(), $database))
+            return call_user_func("FcPDOId::".$database);
+        throw new BadMethodCallException();
+    }
+
+    /**
+      ADD HERE PRIVATE STATIC METHOD FOR EACH OF YOUR DATABASE
+      THESE METHODS CALL LIKE YOUR NAMES OF DATABASES RETURN THEIR CONNEXION ID
+     **/
+
+    /**
+     * @return array
+     */
+    private static function database_test(){
+        return array("database"=>"database_test","host"=>"localhost","user"=>"root","pass"=>"root");
+    }
+}
